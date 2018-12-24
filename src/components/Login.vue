597f1b42080b531fb70c1c5ced2756a272105a87
songying <songying@qq.com>
2018-12-24 19:44:33 +0800

    登录页部分完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="login">这是login页面</div>
+  <div class="login">
+    <div class="login-box"></div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="less">
+// lang="less": 表示使用less写css
+.login {
+  width: 100%;
+  height: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+  .login-box {
+    width: 400px;
+    height: 300px;
+    background-color: #fff;
+    margin: 200px auto;
+    border-radius: 20px;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+// 导入element-ui
+import ElementUI from 'element-ui'
+// 导入element-ui的样式文件
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 导入通用的样式
+import '@/assets/base.css'
+
+// 使用element-ui插件
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
@@ -10,6 +19,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
