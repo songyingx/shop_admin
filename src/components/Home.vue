2becfff3550c9932f3362915918635fbd64c5693
songying <songying@qq.com>
2018-12-25 21:19:02 +0800

    home部分完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,81 @@
 <template>
-  <div class="home">这是Home页面</div>
+  <el-container class="home">
+    <el-header>
+      <div class="logo"></div>
+      <div class="title">
+        <h1>电商管理系统</h1>
+      </div>
+      <div class="logout">
+        <span>欢迎光临</span>
+        <a href="javascript:;" @click="logout">退出</a>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logout() {
+      this.$confirm('您确定要退出本系统吗？', '温馨提示', {
+        type: 'warning'
+      }).then(() => {
+        localStorage.removeItem('token')
+        this.$router.push('/login')
+        this.$message.success('退出系统成功')
+      }).catch(() => {
+        this.$message.info('取消退出')
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.home {
+  height: 100%;
+  .el-header {
+    background-color: #b3c1cd;
+    display: flex;
 
+    .logo {
+      width: 180px;
+      background-image: url("~@/assets/logo.png");
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: center center;
+    }
+
+    .logout {
+      width: 180px;
+      font-weight: 700;
+      text-align: right;
+      line-height: 60px;
+
+      a {
+        color: orange;
+      }
+    }
+
+    .title {
+      flex: 1;
+      h1 {
+        text-align: center;
+        line-height: 60px;
+        color: #fff;
+        font-size: 30px;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
+  .el-main {
+    background-color: #d4dfe4;
+  }
+}
 </style>
