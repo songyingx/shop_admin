92884dbecedf11dce23938ea740cc26fe908f66a
songying <songying@qq.com>
2018-12-27 15:07:55 +0800

    home页面完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,7 +11,42 @@
       </div>
     </el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="/users">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">权限管理</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -23,13 +58,21 @@ export default {
     logout() {
       this.$confirm('您确定要退出本系统吗？', '温馨提示', {
         type: 'warning'
-      }).then(() => {
-        localStorage.removeItem('token')
-        this.$router.push('/login')
-        this.$message.success('退出系统成功')
-      }).catch(() => {
-        this.$message.info('取消退出')
       })
+        .then(() => {
+          localStorage.removeItem('token')
+          this.$router.push('/login')
+          this.$message.success('退出系统成功')
+        })
+        .catch(() => {
+          this.$message.info('取消退出')
+        })
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
@@ -44,7 +87,7 @@ export default {
 
     .logo {
       width: 180px;
-      background-image: url("~@/assets/logo.png");
+      background-image: url('~@/assets/logo.png');
       background-size: contain;
       background-repeat: no-repeat;
       background-position: center center;
